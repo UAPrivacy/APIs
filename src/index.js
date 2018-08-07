8c5c7f3b1952d5838fbe9bbe2d6ae0b1df20cc88
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-08-06 19:34:49 -0700

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-// import sites from "./sites";
-// import update from "./update";
+import { createWriteStream } from 'fs';
+import { get } from 'request';
+import { Parse } from 'unzip';
+import csv2 from 'csv2';
+import { join } from 'path';
 
-// update();
-// sites();
+function writeToFile(arr) {
+  const fileName = join(__dirname, 'data', 'list-1m.json');
+  const file = createWriteStream(fileName);
+  file.on('finish', () => console.log(`writing to ${fileName} complete`));
+  file.on('error', function(err) {
+    throw err;
+  });
+  arr.forEach(function(v) {
+    file.write(v.join(', ') + '\n');
+  });
+  file.end();
+}
+
+function fetch() {
+  return new Promise((resolve, reject) => {
+    const arr = [];
+    get('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
+      .pipe(Parse())
+      .on('entry', entry => {
+        entry.pipe(csv2()).on('data', data => {
+          arr.push(data[1]);
+        });
+      });
+    if (arr.length === 0) {
+      reject(Error`unexpected empty array`);
+    } else {
+      resolve(arr);
+    }
+  });
+}
+
+function main() {
+  fetch()
+    .then(arr => writeToFile(arr))
+    .catch(err => console.error(err));
+}
+main();
