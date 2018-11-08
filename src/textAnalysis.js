dd25a88d7473a944735a21697295a62a44a7aa5b
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-11-08 12:28:20 -0800

    created data tests

--- src/textAnalysis.js
+++ src/textAnalysis.js
@@ -1,14 +1,19 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { getSampleText, SAMPLE_URL } = require('./utils');
-const { MASHAPE } = require('./secrets.json');
+const {
+  getSampleText,
+  FACEBOOK_URL,
+  TWITTER_URL,
+  GOOGLE_URL
+} = require("./utils");
+const { MASHAPE } = require("./secrets.json");
 
-const API = 'text analysis';
+const API = "text analysis";
 const SENT_NUM = 12;
 const ENDPOINT_URL =
-  'https://textanalysis-text-summarization.p.mashape.com/text-summarizer';
+  "https://textanalysis-text-summarization.p.mashape.com/text-summarizer";
 
-async function fetchAxios({ url, text }, context) {
+async function fetch({ url, text }, context) {
   const res = await axios.post(
     ENDPOINT_URL,
     {
@@ -18,9 +23,9 @@ async function fetchAxios({ url, text }, context) {
     },
     {
       headers: {
-        'X-Mashape-Key': MASHAPE,
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        "X-Mashape-Key": MASHAPE,
+        Accept: "application/json",
+        "Content-Type": "application/json"
       }
     }
   );
@@ -30,14 +35,14 @@ async function fetchAxios({ url, text }, context) {
 
 async function main() {
   try {
-    // await fetchAxios({ text: await getSampleText() }, 'text');
-    await fetchAxios(
-      { url: 'http://en.wikipedia.org/wiki/Automatic_summarization' },
-      'URL'
+    const text = await getSampleText();
+    await fetch({ text }, "text");
+    await fetch(
+      { url: "http://en.wikipedia.org/wiki/Automatic_summarization" },
+      "URL"
     );
   } catch (error) {
     console.log(error);
   }
 }
-
 main();
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,35 @@
-const fs = require('fs');
-const util = require('util');
-const { join } = require('path');
+const fs = require("fs");
+const util = require("util");
+const { join } = require("path");
 
 const readFile = util.promisify(fs.readFile);
 
-async function getSampleText() {
-  return await readFile(join(__dirname, 'sample.txt'));
+function read(filename) {
+  return readFile(join(__dirname, filename), "utf-8");
 }
-const SAMPLE_URL = 'https://policies.google.com/terms';
+const getTwitterText = read("twitter.txt");
+const getGoogleText = read("google.txt");
+const getFacebookText = read("facebook.txt");
+
+const GOOGLE_URL = "https://policies.google.com/terms";
+const FACEBOOK_URL = "https://www.facebook.com/terms.php";
+const TWITTER_URL = "https://twitter.com/en/tos";
+
+const SAMPLES = {
+  facebook: {
+    text: getFacebookText,
+    url: FACEBOOK_URL
+  },
+  twitter: {
+    text: getTwitterText,
+    url: TWITTER_URL
+  },
+  google: {
+    text: getGoogleText,
+    url: GOOGLE_URL
+  }
+};
 
 module.exports = {
-  getSampleText,
-  SAMPLE_URL
+  SAMPLES
 };
